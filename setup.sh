41dfe994d08cb7779fd1f0a9b2a5351c2b40340f
AUBENAS Mike <mike.aubenas@gmail.com>
2022-04-26 01:07:30 +0200

    fix: plugin manager for neovim

--- setup.sh
+++ setup.sh
@@ -101,6 +101,11 @@ settingNeoVim ()
 
 	echo "##### NeoVim"
 
+	mkdir -p ~/.vim/bundle
+	cp -f .vimrc ~/.vimrc
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 	mkdir -p ~/.config/nvim/
 	cp -f init.vim ~/.config/nvim/init.vim
 
