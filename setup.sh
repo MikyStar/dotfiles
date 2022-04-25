3e3598646d3445b73a25278b0ba47d298cacb147
AUBENAS Mike <mike.aubenas@gmail.com>
2022-04-26 00:16:17 +0200

    fix: tmux install plugin from script

--- setup.sh
+++ setup.sh
@@ -71,7 +71,7 @@ settingTMUX ()
 
 	cp -f .tmux.conf ~/.tmux.conf
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-	tmux source ~/.tmux.conf
+	~/.tmux/plugins/tpm/scripts/install_plugins.sh
 
 	cd ~/.tmux/plugins/tmux-mem-cpu-load
 	cmake .
